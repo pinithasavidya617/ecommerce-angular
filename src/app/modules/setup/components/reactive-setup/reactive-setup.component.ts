71e7cb712f3341d02d90acaaae02d18f5323473d
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-07-07 21:51:55 +0530

    Add reactive forms

---
+++ src/app/modules/setup/components/reactive-setup/reactive-setup.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-reactive-setup',
+  templateUrl: './reactive-setup.component.html',
+  styleUrls: ['./reactive-setup.component.scss']
+})
+export class ReactiveSetupComponent {
+
+  nameField= new FormControl('Amal', [Validators.required, Validators.minLength(6)]);
+
+  addressForm = new FormGroup(
+    {
+      street: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      zipcode: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+    }
+  )
+  constructor() {
+  }
+
+  onSubmit() {
+    this.addressForm.markAsTouched();
+    if(this.addressForm.valid) {
+      console.log("Valid address");
+    }else {
+      console.log("InValid address");
+    }
+  }
+
+
+}
